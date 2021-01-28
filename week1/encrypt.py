77c33ba61fdb57e5704f967ed7f29be7b77df8e0
hmoon630 <s19017@gsm.hs.kr>
2021-01-28 13:15:14 +0900

    20210128

--- week1/encrypt.py
+++ week1/encrypt.py
@@ -2,26 +2,44 @@ import boto3
 from botocore.config import Config
 
 import argparse
+from time import time, sleep
 
 
+def logger(func):
+    def log(*args, **kwargs):
+        start = time()
+        print(f"start {func.__name__}...")
+        result = func(*args, **kwargs)
+        print(f"Done! Took {round(time() - start, 2)} sec")
+        return result
+
+    return log
+
+
+@logger
 def main():
     instance_id = parse_arg()
     ec2 = boto3.resource("ec2")
 
-    # instance = get_instance(ec2, instance_id)
-    # volume = get_volume_from_instance(instance)
-    # snapshot = create_snapshot(volume)
+    instance = get_instance(ec2, instance_id)
+    volume = get_volume_from_instance(instance)
+    snapshot = create_snapshot(volume)
 
-    # encrypted_snapshot = copy_and_encrypt_snapshot(ec2, snapshot)
+    encrypted_snapshot = copy_and_encrypt_snapshot(ec2, snapshot)
 
-    encrypted_snapshot = ec2.Snapshot("snap-0d776d128b0ed90ac")
-    encrypted_volume = create_volume_with_snapshot(ec2, encrypted_snapshot)
-    print(encrypted_volume)
+    encrypted_volume = create_volume_with_snapshot(
+        ec2, encrypted_snapshot, instance.placement["AvailabilityZone"]
+    )
 
+    stopped = stop_instance(instance)
+    detach_volume_from_instance(instance, volume)
+    attach_volume_to_instance(instance, encrypted_volume)
+    start_instance(instance)
     pass
 
 
 # Get instance id from args
+@logger
 def parse_arg():
     args_parser = argparse.ArgumentParser()
     args_parser.add_argument("--instance-id")
@@ -32,16 +50,19 @@ def parse_arg():
 
 
 # Get instance by id
+@logger
 def get_instance(ec2, instance_id):
     return ec2.Instance(instance_id)
 
 
 # Get root volume
+@logger
 def get_volume_from_instance(instance):
     return list(instance.volumes.all())[0]
 
 
 # Create snapshot
+@logger
 def create_snapshot(volume):
     snapshot = volume.create_snapshot()
     snapshot.wait_until_completed()
@@ -50,6 +71,7 @@ def create_snapshot(volume):
 
 
 # Copy snapshot & encrypt
+@logger
 def copy_and_encrypt_snapshot(ec2, snapshot):
     copied_snapshot = snapshot.copy(
         Encrypted=True, KmsKeyId="alias/aws/ebs", SourceRegion="ap-northeast-2"
@@ -57,9 +79,12 @@ def copy_and_encrypt_snapshot(ec2, snapshot):
     new_snapshot = ec2.Snapshot(copied_snapshot["SnapshotId"])
     new_snapshot.wait_until_completed()
 
+    return new_snapshot
+
 
 # Create volume
-def create_volume_with_snapshot(ec2, snapshot):
+@logger
+def create_volume_with_snapshot(ec2, snapshot, availability_zone):
     volume = ec2.create_volume(
         AvailabilityZone="ap-northeast-2a", SnapshotId=snapshot.id
     )
@@ -67,16 +92,45 @@ def create_volume_with_snapshot(ec2, snapshot):
     return volume
 
 
-# Create new instance
-def create_new_instance(ec2, name, snapshot):
-    new_instance = ec2.create_instance(
-        BlockDeviceMappings=[{"VirtualName": name, "Ebs": {"SnapshotId": snapshot.id}}]
-    )
+# Stop Instance
+@logger
+def stop_instance(instance):
+    instance.stop()
+    instance.wait_until_stopped()
 
-    return new_instance
+    return instance
 
 
-# Attach EIP to new instance
+# Detach volume from instance
+@logger
+def detach_volume_from_instance(instance, volume):
+    instance.detach_volume(VolumeId=volume.id)
+
+    while volume.state == "in-use":
+        sleep(3)
+        volume.reload()
+
+    volume.delete()
+
+    return instance
+
+
+# Attach volume to instance
+@logger
+def attach_volume_to_instance(instance, volume):
+    instance.attach_volume(Device="/dev/xvda", VolumeId=volume.id)
+
+    return instance
+
+
+# Start Instance
+@logger
+def start_instance(instance):
+    instance.start()
+    instance.wait_until_running()
+
+    return instance
+
 
 if __name__ == "__main__":
     main()
