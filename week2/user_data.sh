e5adf58c773bfa356e4e8ed55e23025b3da185d1
hmoon630 <s19017@gsm.hs.kr>
2021-02-05 17:45:29 +0900

    2021-02-05

--- week2/user_data.sh
+++ week2/user_data.sh
@@ -5,4 +5,4 @@ cd /home/ec2-user
 git clone https://github.com/hmoon630/aws-cloud.git
 cd aws-cloud/week2
 pip3 install boto3
-python ./app.py > /home/ec2-user/result.txt
+python3 ./app.py > /home/ec2-user/result.txt
